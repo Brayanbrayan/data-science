16a4230ab646f70ab5af679b6c36c4e22b188973
Brayan <bnjoroge259@gmail.com>
2025-02-13 11:43:46 +0300

    Worked with Dispersion(range,variace and standard deviation)

--- src/statisticss.py
+++ src/statisticss.py
@@ -88,3 +88,42 @@ def mode(x: List[float]) -> List[float]:
             if count == max_count]
 
 assert set(mode(num_friends)) == {1, 6}
+
+
+
+# "range" already means something in Python, so we'll use a different name
+def data_range(xs: List[float]) -> float:
+    return max(xs) - min(xs)
+
+assert data_range(num_friends) == 99
+
+from scratch.linear_algebra import sum_of_squares
+
+def de_mean(xs: List[float]) -> List[float]:
+    """Translate xs by subtracting its mean (so the result has mean 0)"""
+    x_bar = mean(xs)
+    return [x - x_bar for x in xs]
+
+def variance(xs: List[float]) -> float:
+    """Almost the average squared deviation from the mean"""
+    assert len(xs) >= 2, "variance requires at least two elements"
+
+    n = len(xs)
+    deviations = de_mean(xs)
+    return sum_of_squares(deviations) / (n - 1)
+
+assert 81.54 < variance(num_friends) < 81.55
+
+import math
+
+def standard_deviation(xs: List[float]) -> float:
+    """The standard deviation is the square root of the variance"""
+    return math.sqrt(variance(xs))
+
+assert 9.02 < standard_deviation(num_friends) < 9.04
+
+def interquartile_range(xs: List[float]) -> float:
+    """Returns the difference between the 75%-ile and the 25%-ile"""
+    return quantile(xs, 0.75) - quantile(xs, 0.25)
+
+assert interquartile_range(num_friends) == 6
